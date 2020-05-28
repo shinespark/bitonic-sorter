69609987e9e4d6008999cb6635acdaa36252bde5
shinespark <shnsprk@gmail.com>
2020-05-28 13:41:46 +0900

    compare_and_swapを実装

--- src/first.rs
+++ src/first.rs
@@ -2,8 +2,8 @@ pub fn sort(x: &mut [u32], up: bool) {
     if x.len() > 1 {
         let mid_point = x.len() / 2;
         sort(&mut x[..mid_point], true);
-        sort(&mut x[..mid_point], false);
-        sub_sort(x, up)
+        sort(&mut x[mid_point..], false);
+        sub_sort(x, up);
     }
 }
 
@@ -17,5 +17,33 @@ fn sub_sort(x: &mut [u32], up: bool) {
 }
 
 fn compare_and_swap(x: &mut [u32], up: bool) {
-    unimplemented!();
+    let mid_point = x.len() / 2;
+    for i in 0..mid_point {
+        if (x[i] > x[mid_point + i]) == up {
+            // 要素を交換する
+            x.swap(i, mid_point + i);
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    // 親モジュール(first)のsort関数を使用する
+    use super::sort;
+
+    #[test]
+    fn sort_u32_ascending() {
+        let mut x = vec![10, 30, 11, 20, 4, 330, 21, 110];
+        sort(&mut x, true);
+
+        assert_eq!(x, vec![4, 10, 11, 20, 21, 30, 110, 330]);
+    }
+
+    #[test]
+    fn sort_u32_descending() {
+        let mut x = vec![10, 30, 11, 20, 4, 330, 21, 110];
+        sort(&mut x, false);
+
+        assert_eq!(x, vec![330, 110, 30, 21, 20, 11, 10, 4]);
+    }
 }
