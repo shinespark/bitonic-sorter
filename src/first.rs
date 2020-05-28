de5016a6f5118a058c4548aece1e738332050660
shinespark <shnsprk@gmail.com>
2020-05-28 13:31:41 +0900

    add first.rs

---
+++ src/first.rs
@@ -0,0 +1,12 @@
+pub fn sort(x: &mut [u32], up: bool) {
+    // 未実装を示すマクロ
+    unimplemented!();
+}
+
+fn sub_sort(x: &mut [u32], up: bool) {
+    unimplemented!();
+}
+
+fn compare_and_swap(x: &mut [u32], up: bool) {
+    unimplemented!();
+}
