16827afe2d516834f9ce4d1cf75dd57f3ebff13f
shinespark <shnsprk@gmail.com>
2020-05-28 13:37:06 +0900

    sort関数を実装

--- src/first.rs
+++ src/first.rs
@@ -1,9 +1,14 @@
 pub fn sort(x: &mut [u32], up: bool) {
-    // 未実装を示すマクロ
-    unimplemented!();
+    if x.len() > 1 {
+        let mid_point = x.len() / 2;
+        sort(&mut x[..mid_point], true);
+        sort(&mut x[..mid_point], false);
+        sub_sort(x, up)
+    }
 }
 
 fn sub_sort(x: &mut [u32], up: bool) {
+    // 未実装を示すマクロ
     unimplemented!();
 }
 
