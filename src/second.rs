cb14941a083609317cd1f898c482cea8dc133d3c
shinespark <shnsprk@gmail.com>
2020-05-29 11:06:34 +0900

    add src test

--- src/second.rs
+++ src/second.rs
@@ -46,4 +46,62 @@ mod tests {
 
         assert_eq!(x, vec![330, 110, 30, 21, 20, 11, 10, 4]);
     }
+
+    #[test]
+    fn sort_str_ascending() {
+        let mut x = vec![
+            "Rust",
+            "is",
+            "fast",
+            "and",
+            "memory-efficient",
+            "with",
+            "no",
+            "GC",
+        ];
+        sort(&mut x, true);
+
+        assert_eq!(
+            x,
+            vec![
+                "GC",
+                "Rust",
+                "and",
+                "fast",
+                "is",
+                "memory-efficient",
+                "no",
+                "with",
+            ]
+        );
+    }
+
+    #[test]
+    fn sort_str_descending() {
+        let mut x = vec![
+            "Rust",
+            "is",
+            "fast",
+            "and",
+            "memory-efficient",
+            "with",
+            "no",
+            "GC",
+        ];
+        sort(&mut x, false);
+
+        assert_eq!(
+            x,
+            vec![
+                "with",
+                "no",
+                "memory-efficient",
+                "is",
+                "fast",
+                "and",
+                "Rust",
+                "GC",
+            ]
+        );
+    }
 }
