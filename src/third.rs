3766bea9e8f5be9579335cd604e14346b96ac741
shinespark <shnsprk@gmail.com>
2020-06-01 10:36:31 +0900

    copy second to third

---
+++ src/third.rs
@@ -0,0 +1,142 @@
+use super::SortOrder;
+
+pub fn sort<T: Ord>(x: &mut [T], order: &SortOrder) -> Result<(), String> {
+    if x.len().is_power_of_two() {
+        // * はポインタ？
+        match *order {
+            SortOrder::Ascending => do_sort(x, true),
+            SortOrder::Descending => do_sort(x, false),
+        }
+        Ok(())
+    } else {
+        Err(format!(
+            "The length of x is not a power of two. (x.len(): {}",
+            x.len()
+        ))
+    }
+}
+
+fn do_sort<T: Ord>(x: &mut [T], up: bool) {
+    if x.len() > 1 {
+        let mid_point = x.len() / 2;
+        do_sort(&mut x[..mid_point], true);
+        do_sort(&mut x[mid_point..], false);
+        sub_sort(x, up);
+    }
+}
+
+fn sub_sort<T: Ord>(x: &mut [T], up: bool) {
+    if x.len() > 1 {
+        compare_and_swap(x, up);
+        let mid_point = x.len() / 2;
+        sub_sort(&mut x[..mid_point], up);
+        sub_sort(&mut x[mid_point..], up);
+    }
+}
+
+fn compare_and_swap<T: Ord>(x: &mut [T], up: bool) {
+    let mid_point = x.len() / 2;
+    for i in 0..mid_point {
+        if (x[i] > x[mid_point + i]) == up {
+            // 要素を交換する
+            x.swap(i, mid_point + i);
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    // 親モジュール(first)のsort関数を使用する
+    use super::sort;
+    use crate::SortOrder::*;
+
+    #[test]
+    fn sort_u32_ascending() {
+        let mut x: Vec<u32> = vec![10, 30, 11, 20, 4, 330, 21, 110];
+        assert_eq!(sort(&mut x, &Ascending), Ok(()));
+        assert_eq!(x, vec![4, 10, 11, 20, 21, 30, 110, 330]);
+    }
+
+    #[test]
+    fn sort_u32_descending() {
+        let mut x: Vec<u32> = vec![10, 30, 11, 20, 4, 330, 21, 110];
+        assert_eq!(sort(&mut x, &Descending), Ok(()));
+        assert_eq!(x, vec![330, 110, 30, 21, 20, 11, 10, 4]);
+    }
+
+    #[test]
+    fn sort_str_ascending() {
+        let mut x = vec![
+            "Rust",
+            "is",
+            "fast",
+            "and",
+            "memory-efficient",
+            "with",
+            "no",
+            "GC",
+        ];
+        assert_eq!(sort(&mut x, &Ascending), Ok(()));
+        assert_eq!(
+            x,
+            vec![
+                "GC",
+                "Rust",
+                "and",
+                "fast",
+                "is",
+                "memory-efficient",
+                "no",
+                "with",
+            ]
+        );
+    }
+
+    #[test]
+    fn sort_str_descending() {
+        let mut x = vec![
+            "Rust",
+            "is",
+            "fast",
+            "and",
+            "memory-efficient",
+            "with",
+            "no",
+            "GC",
+        ];
+        assert_eq!(sort(&mut x, &Descending), Ok(()));
+        assert_eq!(
+            x,
+            vec![
+                "with",
+                "no",
+                "memory-efficient",
+                "is",
+                "fast",
+                "and",
+                "Rust",
+                "GC",
+            ]
+        );
+    }
+
+    // #[test]
+    // fn sort_f64() {
+    //     let mut x = vec![20.0, -30.0, 11.0, 10.0];
+    //     sort(&mut x, &Ascending);
+    // }
+    // #[test]
+    // fn sort_mixed() {
+    //     // 実行されないがコンパイル時にエラーが検知される
+    //     if false {
+    //         let mut x = vec![10, 30, "a", "b"];
+    //         sort(&mut x, &Ascending);
+    //     }
+    // }
+
+    #[test]
+    fn sort_to_fail() {
+        let mut x = vec![10, 30, 11];
+        assert!(sort(&mut x, &Ascending).is_err());
+    }
+}
