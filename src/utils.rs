51c0a2486f80004a2047aaf1a2cbea38a7eb44d2
shinespark <shnsprk@gmail.com>
2020-06-04 10:00:58 +0900

    イテレータチェインに書き換え

--- src/utils.rs
+++ src/utils.rs
@@ -6,10 +6,6 @@ pub fn new_u32_vec(n: usize) -> Vec<u32> {
     // RNGの初期化。再現性を持たせるためにシード値を固定
     let mut rng = Pcg64Mcg::from_seed([0; 16]);
 
-    let mut v = Vec::with_capacity(n);
-    // 0からn-1までのn回繰り返し乱数を生成する
-    for _ in 0..n {
-        v.push(rng.sample(&Standard));
-    }
-    v
+    // イテレータチェイン
+    rng.sample_iter(&Standard).take(n).collect()
 }
